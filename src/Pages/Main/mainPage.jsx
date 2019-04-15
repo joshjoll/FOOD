be90fca2934a163aaec3682fcd2c2e5a393acb48
joshjoll <josh.joll@gmail.com>
2019-04-15 17:32:17 -0500

    recipes filtering, returning, passing to state, and rendering

--- src/Pages/Main/mainPage.jsx
+++ src/Pages/Main/mainPage.jsx
@@ -1,26 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import IngredientArea from '../../Components/IngredientArea/IngredientArea';
 import RecipeArea from '../../Components/RecipeArea/RecipeArea';
+import recipeService from '../../utils/recipeService';
 
 
-const MainPage = (props) => {
-  return (
-    <div className="mainPage">
-      <IngredientArea
-        ingredients={props.ingredients}
-        text={props.text}
-        handleChange={props.handleChange}
-        handleSubmit={props.handleSubmit}
-        handleRemove={props.handleRemove}
-      />
-      <RecipeArea
-      recipeMatch={props.recipeMatch}
-      ingredients={props.ingredients}
-      />
-    </div>
-  );
+class MainPage extends Component {
 
-};
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      ingredients: [],
+      recipeMatch: [],
+      isSearched : false
+
+    };
+  }
+
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      ingredients: this.state.ingredients.concat([this.state.text]),/*[...this.state.ingredients, this.state.text],*/
+      text: ''
+    }, async () => {
+      const res  = await recipeService.filterRecipe(this.state.ingredients)
+      // console.log(res[0].recipeName)
+      const result = [...res]
+       this.setState({
+        recipeMatch : result
+      })
+
+      ;
+      /*this.setState({receipeMatch: this.state.recipeMatch.concat(res) });
+      console.log(this.state.recipeMatch);
+      return res;*/
+    },
+  )
+  };
+
+
+  handleRemove = e => {
+    // e.preventDefault();
+    let array = [...this.state.ingredients]
+    let idx = array.indexOf(e);
+    array.splice(idx, 1);
+    this.setState({ ingredients: array }, async () => {
+      const res  = await recipeService.filterRecipe(this.state.ingredients)
+      // console.log(res[0].recipeName)
+      const result = [...res]
+       this.setState({
+        recipeMatch : result
+      });
+    })
+  }
+
+
+  render() {
+    //move your map here
+
+    return (
+      <div className="mainPage">
+        <IngredientArea
+          ingredients={this.state.ingredients}
+          text={this.state.text}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          handleRemove={this.handleRemove}
+        />
+
+        {this.state.recipeMatch ?
+          <div className="recipeArea">
+            {this.state.recipeMatch.map((recipe, index) =>
+              <div className="recipe" key={index}>
+                <img src={recipe.recipeImg} />
+                <p><a href={recipe.recipeLink} target="_blank">{recipe.recipeName}</a></p>
+
+              </div>
+          )}
+          </div>
+          :
+          <div>
+          <p> nothing was found </p>
+          </div>
+      }
+      </div>
+    );
+  }
+}
+
 
 export default MainPage;
