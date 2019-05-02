e682d4ba789a794d0e566742e36499545c6fa2a5
joshjoll <josh.joll@gmail.com>
2019-05-02 14:03:07 -0500

    test

--- src/Components/UserPreferences/UserPreferences.jsx
+++ src/Components/UserPreferences/UserPreferences.jsx
@@ -6,12 +6,13 @@ import '../../Pages/User/ProfilePage.css';
 const UserPreferences = (props) => {
   return (
     <div className="userPreferences">
+    <h3>Your Dietary Preferences</h3>
     <ul>
     <li>Ketogenic? {props.user.ketogenic ? "Yes Please" : 'Nope'}</li>
     <li>Dairy Free? {props.user.dairyFree ? "Yes Please" : 'Nope'}</li>
     <li>Vegetarian? {props.user.vegetarian ? "Yes Please" : 'Nope'}</li>
     <li>Vegan? {props.user.vegan ? "Yes Please" : 'Nope'}</li>
-    <li>Gluten Free?{props.user.glutenFree ? "Yes Please" : 'Nope'}</li>
+    <li>Gluten Free? {props.user.glutenFree ? "Yes Please" : 'Nope'}</li>
     </ul>
     </div>
   );
